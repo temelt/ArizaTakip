958284594e6861f3b0a2d91fd64bc9b33ef28068
temelt <tanertemel@outlook.com>
2018-05-12 11:28:53 +0300

    pagination

--- ArizaTakip/src/main/java/com/temelt/arizatakip/view/CagriController.java
+++ ArizaTakip/src/main/java/com/temelt/arizatakip/view/CagriController.java
@@ -2,11 +2,16 @@ package com.temelt.arizatakip.view;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 
 import com.temelt.arizatakip.entity.Cagri;
@@ -28,13 +33,14 @@ public class CagriController implements Serializable {
 	@Autowired
 	private PersonelService personelService;
 	
-	private List<Cagri> cagriListe ;
+	private LazyDataModel<Cagri> cagriListe ;
 	private Cagri cagri;
 
 	@PostConstruct
 	private void init(){
-		cagriListe = cagriService.findAll();
+		listele();
 		yeni();
+		
 	}
 	
 	public List<Personel> personelAcomp(String term) {
@@ -52,16 +58,56 @@ public class CagriController implements Serializable {
 	
 	public void kaydet(){
 		cagriService.save(cagri);
-		cagriListe = cagriService.findAll();
+		listele();
 		yeni();
 	}
 	
 	public void sil(Long id){
 		cagriService.delete(id);
-		cagriListe = cagriService.findAll();
+		listele();
 	}
 	
-	public List<Cagri> getCagriListe() {
+	public void listele(){
+		cagriListe =new LazyDataModel<Cagri>() {
+			
+			List<Cagri> list;
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 6120622133867511444L;
+			
+			@Override
+			public List<Cagri> load(int first, int pageSize, String sortField, SortOrder sortOrder,
+					Map<String, Object> filters) {
+				int offset=0;
+				if(first>0) {
+					offset = first/pageSize;
+				}
+				Page<Cagri> liste = cagriService.findAll(new PageRequest(offset, pageSize,null));
+				this.setRowCount(Integer.parseInt(String.valueOf(liste.getTotalElements())));
+				list = liste.getContent();
+				return list;
+			}
+			
+		    @Override
+		    public Cagri getRowData(String rowKey) {
+		        for(Cagri c : list) {
+		            if(c.getId().equals(rowKey))
+		                return c;
+		        }
+		 
+		        return null;
+		    }
+		 
+		    @Override
+		    public Object getRowKey(Cagri c) {
+		        return c.getId();
+		    }
+			
+		};
+	}
+	
+	public LazyDataModel<Cagri> getCagriListe() {
 		return cagriListe;
 	}
 	
