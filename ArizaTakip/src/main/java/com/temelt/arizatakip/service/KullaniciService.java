c0a59ff20b501d8a27b6fc0e108f97bd5e015333
user <user@LAPTOP-HLJTUSJ8>
2018-05-13 12:18:16 +0300

    njbnjb

--- ArizaTakip/src/main/java/com/temelt/arizatakip/service/KullaniciService.java
+++ ArizaTakip/src/main/java/com/temelt/arizatakip/service/KullaniciService.java
@@ -1,10 +1,12 @@
 package com.temelt.arizatakip.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.temelt.arizatakip.entity.Kullanici;
 @Repository
 public interface KullaniciService extends JpaRepository<Kullanici,Long>{
-
+ Page<Kullanici> findByUsrnmContainingIgnoreCase(String usrnm,Pageable pageable);
 }
--- ArizaTakip/src/main/java/com/temelt/arizatakip/view/KullaniciController.java
+++ ArizaTakip/src/main/java/com/temelt/arizatakip/view/KullaniciController.java
@@ -2,13 +2,20 @@ package com.temelt.arizatakip.view;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 
+import com.temelt.arizatakip.entity.Cagri;
 import com.temelt.arizatakip.entity.Kisi;
 import com.temelt.arizatakip.entity.Kullanici;
 import com.temelt.arizatakip.service.KisiService;
@@ -26,7 +33,7 @@ public class KullaniciController implements Serializable {
 	private KullaniciService kservice;
 	@Autowired
 	private KisiService kisiservice;
-	private List<Kullanici> kullaniciL;
+	private LazyDataModel<Kullanici> kullaniciL;
 	private Kullanici kullanici;
 
 	public void yeni() {
@@ -46,13 +53,13 @@ public class KullaniciController implements Serializable {
 		this.kullanici = kullanici;
 	}
 
-	public List<Kullanici> getkullaniciL() {
+	public LazyDataModel<Kullanici> getkullaniciL() {
 		return kullaniciL;
 	}
 
 	@PostConstruct
 	private void init() {
-		kullaniciL = kservice.findAll();
+		listele();
 		yeni();
 	}
 
@@ -62,13 +69,76 @@ public class KullaniciController implements Serializable {
 
 	public void save() {
 		kservice.save(kullanici);
-		kullaniciL = kservice.findAll();
+		listele();
 		yeni();
 	}
 
 	public void delete(Long id) {
 		kservice.delete(id);
-		kullaniciL = kservice.findAll();
+		listele();
 	}
 
-}
+	public void listele() {
+		kullaniciL=new LazyDataModel<Kullanici>() {
+			List<Kullanici> klist;
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public List<Kullanici> load(int first, int pageSize, String sortField, SortOrder sortOrder,
+					Map<String, Object> filters) {
+				int offset=0;
+				if(first>0) {
+					offset = first/pageSize;
+				}
+				
+				Sort s=null;
+				if(sortOrder!=null && sortField !=null) {
+					if(sortOrder.equals(SortOrder.ASCENDING)) {
+						s =new Sort(Sort.Direction.ASC,sortField);
+					}else if(sortOrder.equals(SortOrder.DESCENDING)) {
+						s =new Sort(Sort.Direction.DESC,sortField);
+					}
+				}
+							
+				String usrnm="";
+				if(filters.get("usrnm")!=null) {
+					usrnm=filters.get("usrnm").toString();
+				}
+				
+				Page<Kullanici> liste = kservice.findByUsrnmContainingIgnoreCase(usrnm,new PageRequest(offset, pageSize,s));
+				this.setRowCount(Integer.parseInt(String.valueOf(liste.getTotalElements())));
+				klist = liste.getContent();
+				return klist;
+			}
+			
+		    @Override
+		    public Kullanici getRowData(String rowKey) {
+		        for(Kullanici c : klist) {
+		            if(c.getId().equals(rowKey))
+		                return c;
+		        }
+		 
+		        return null;
+		    }
+		 
+		    @Override
+		    public Object getRowKey(Kullanici c) {
+		        return c.getId();
+		    }
+			
+		};
+	}
+			
+		
+		
+		
+		
+		
+		
+	}
+
+
